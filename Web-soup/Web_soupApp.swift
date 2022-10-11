a81b35e7602e7aaeeb7e2b0b8238e2b0de4fedef
joshua kilaha <jokilaha191@gmail.com>
2022-10-11 17:10:53 +0300

    Initial Commit

---
+++ Web-soup/Web_soupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Web_soupApp.swift
+//  Web-soup
+//
+//  Created by mroot on 11/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Web_soupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
